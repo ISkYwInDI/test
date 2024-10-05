5d4a1080254e74a6e719eebd7708ce008420d947
ISkYwInDI <izimov04@mail.ru>
2024-10-05 16:02:04 +0500

    Добавлена базовая логика программы

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main ()
+    {
+        try
+        {
+            Console.Write("Введите число x: ");
+            double x = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите число y: ");
+            double y = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите число z: ");
+            double z = Convert.ToDouble(Console.ReadLine());
+            double max = Math.Max(x, Math.Max(y, z));
+            Console.WriteLine($"Максимальное значение: {max}"); 
+        }
+        catch (Exception ex)
+        { 
+            Console.WriteLine (ex.Message);
+        }
+    }
+}
